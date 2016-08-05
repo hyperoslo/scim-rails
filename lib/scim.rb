b36b02d972d972b478f00260c3c6b88bc57fdb6f
Felipe Espinoza <fespinozacast@gmail.com>
2016-08-05 14:46:36 +0200

    Fix rubocop issues with the gem code.

--- lib/scim.rb
+++ lib/scim.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'scim/engine'
 
 # initial module definition for SCIM
--- lib/scim/engine.rb
+++ lib/scim/engine.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module SCIM
   # rails engine
   class Engine < ::Rails::Engine
