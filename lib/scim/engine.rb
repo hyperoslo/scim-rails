1c860bec3006f3dd15546c6250b02ea4ada35c27
Felipe Espinoza <fespinozacast@gmail.com>
2016-08-05 10:08:51 +0200

    Add rspec gem with rake and dummy project setup.

--- lib/scim/engine.rb
+++ lib/scim/engine.rb
@@ -2,5 +2,12 @@ module SCIM
   # rails engine
   class Engine < ::Rails::Engine
     isolate_namespace SCIM
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.assets false
+      g.helper false
+    end
   end
 end
